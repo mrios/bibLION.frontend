a89e802f0e3d52e9190792628110cbe509b2b2bc
mrios <riosmatias.zarate@gmail.com>
2014-04-24 19:03:13 -0300

    New Sigma.Abm > Loans

--- app/loans/controls/loans.js
+++ app/loans/controls/loans.js
@@ -1,20 +1,119 @@
 steal(
-	'sigma/controls/control'
+	'sigma/controls/abm'
 ,	'app/common/models/Loan.js'
 ).then(
 	function()
 	{
-		Sigma.Control(
+		Sigma.Abm(
 			'Bib.Loans'
 		,	{
 				defaults:
 				{
-					view_list:	'app/loans/views/list.mustache'
+					view_table:	'app/loans/views/list.mustache'
 				,	name: 'Prestamo'
-				,	data: Loan.filter
+				,	data:	
+					{
+						title: 'Prestamos'
+					}
+				,	titles:
+					{
+						create: 	'Crear Prestamo'
+					,	update: 	'Editar Prestamo'
+					}
+				,	messages:
+					{
+						created: 	"Prestamo creado satisfactoriamiente."
+					,	updated: 	"Prestamo editado satisfactoriamiente."
+					,	deleted: 	"Prestamo removido satisfactoriamiente."
+					,	failed: 	undefined
+					,	modal_delete: 'Si elimina el Prestamo, no podrá recuperarlo'
+					}
+				,	table_data:
+					{
+						paginable: 	{
+										limit:		5
+									,	maxIndex:	7
+									,	offset:		0
+									}
+					,	searcheable:'nombre'
+					,	sorteable:  true
+					}
+				,	model: Loan
+				,	form_data:
+					[
+						{
+							type:	'text'
+						,	name:	'lastName'
+						,	label:	'Apellido'
+						,	required: true 
+						}
+					,	{
+							type:	'text'
+						,	name:	'name'
+						,	label:	'Nombre' 
+						}
+					,	{
+							type: 'button'
+						,	class: 'btn-primary btn-md'
+						,	name: 'save'
+						,	label: 'Guardar'
+						}
+					]
+				,	data:
+					{
+						title: 'ABM de Prestamos'
+					}
+				,	route:
+					{
+						route:	'/:option/:abm'
+					,	abm:	'list'
+					}
 				}
 			}
 		,	{
+				quickSearch: function(query)
+				{
+					return	_.isEmpty(query.value)
+							?	{}
+							:	{
+									operator: 'or'
+								,	filters:
+									_.union(
+										[
+											{
+												field: 'nombre'
+											,	value: query.value.toUpperCase()
+											,	criteria: '%'
+											}
+										]
+									,	!_.isNaN(parseInt(query.value))
+										?	[
+												{
+													field: 'nroCentro'
+												,	value: query.value
+												,	criteria: '='
+												}
+											]
+										:	[]
+									)
+								}
+				}
+
+			,	submitForm: function(instance,formData)
+				{
+					var	toSave
+					=	{
+							idNombreEmpresa: formData.attr('nombreEmpresa.data.id')
+						,	nombre: formData.attr('nombre.value')
+						,	nroCentro: formData.attr('nroCentro.value')
+						,	telefono: formData.attr('telefono.value')
+						,	email: formData.attr('email.value')
+						,	observaciones: formData.attr('observaciones.value')
+						}
+					return	_.isUndefined(instance)
+							?	new Milkrun.Models.CentroCosto(toSave).save()
+							:	instance.attr(toSave).save()
+				}
 			}
 		)
 	}
